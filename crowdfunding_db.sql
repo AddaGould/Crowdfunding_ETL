080fd03077f157a6d70a683f633f01d00e67f8d0
ANAHI <anahi.jaimes2020@gmail.com>
2024-02-01 21:35:43 -0600

    Final Draft

--- crowdfunding_db.sql
+++ crowdfunding_db.sql
@@ -1,39 +1,34 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/5WBvhJ
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
-
 CREATE TABLE "campaign" (
     "cf_id" INT   NOT NULL,
     "contact_id" INT   NOT NULL,
-    "company_name" VARCHAR(50)   NOT NULL,
-    "description" STRING(100)   NOT NULL,
+    "company_name" VARCHAR(255)   NOT NULL,
+    "description" VARCHAR(255)   NOT NULL,
     "goal" FLOAT   NOT NULL,
     "pledged" FLOAT   NOT NULL,
-    "outcome" CHAR(10)   NOT NULL,
+    "outcome" VARCHAR(255)   NOT NULL,
     "backers_count" INT   NOT NULL,
-    "country" CHAR(10)   NOT NULL,
-    "currency" CHAR(10)   NOT NULL,
+    "country" VARCHAR(255)   NOT NULL,
+    "currency" VARCHAR(255)   NOT NULL,
     "launch_date" DATE   NOT NULL,
     "end_date" DATE   NOT NULL,
-    "category_id" VARCHAR(10)   NOT NULL,
-    "subcategory_id" VARCHAR(10)   NOT NULL,
+    "category_id" VARCHAR(255)   NOT NULL,
+    "subcategory_id" VARCHAR(255)   NOT NULL,
     CONSTRAINT "pk_campaign" PRIMARY KEY (
         "cf_id"
      )
 );
 
 CREATE TABLE "category" (
-    "category_id" VARCHAR(10)   NOT NULL,
-    "category" STRING(20)   NOT NULL,
+    "category_id" VARCHAR(255)   NOT NULL,
+    "category" VARCHAR(255)   NOT NULL,
     CONSTRAINT "pk_category" PRIMARY KEY (
         "category_id"
      )
 );
 
 CREATE TABLE "subcategory" (
-    "subcategory_id" VARCHAR(10)   NOT NULL,
-    "subcategory" STRING(20)   NOT NULL,
+    "subcategory_id" VARCHAR(255)   NOT NULL,
+    "subcategory" VARCHAR(255)   NOT NULL,
     CONSTRAINT "pk_subcategory" PRIMARY KEY (
         "subcategory_id"
      )
@@ -41,9 +36,9 @@ CREATE TABLE "subcategory" (
 
 CREATE TABLE "contacts" (
     "contact_id" INT   NOT NULL,
-    "first_name" STRING(30)   NOT NULL,
-    "last_name" STRING(30)   NOT NULL,
-    "email" VARCHAR(50)   NOT NULL,
+    "first_name" VARCHAR(255)   NOT NULL,
+    "last_name" VARCHAR(255)   NOT NULL,
+    "email" VARCHAR(255)   NOT NULL,
     CONSTRAINT "pk_contacts" PRIMARY KEY (
         "contact_id"
      )
@@ -58,3 +53,7 @@ REFERENCES "category" ("category_id");
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
 REFERENCES "subcategory" ("subcategory_id");
 
+SELECT * FROM campaign;
+SELECT * FROM category;
+SELECT * FROM subcategory;
+SELECT * FROM contacts;
